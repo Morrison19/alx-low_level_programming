7bd705b5f46d0c4aad776ee829397e8b39eacc1d
Morrison <Ohiwereibolade72@gmail.com>
2022-09-12 15:15:13 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - prints all single digitd of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
