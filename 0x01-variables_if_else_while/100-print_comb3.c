7385132a0c0cef75179474795ba85d67baca1d2d
Morrison <Ohiwereibolade72@gmail.com>
2022-09-12 17:24:09 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description -  prints all possible different combinations of two digits
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Print only the smallest combination of two digits
+ * You can only use putchar five times maximum in your code
+ * You are not allowed to use any variable of type char
+ */
+
+int main(void)
+{
+
+	int i, j, k;
+
+	i = 0;
+
+	while (i < 100)
+	{
+		j = i % 10; /* singles digit */
+		k = i / 10; /* doubles digit */
+
+		if (k < j)
+		{
+			putchar(k + '0');
+			putchar(j + '0');
+
+			if (i < 89)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
