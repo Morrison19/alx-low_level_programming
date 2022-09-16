618dea44068f370e42e0ed2b48e6e60c41b4a0bb
Morrison <Ohiwereibolade72@gmail.com>
2022-09-16 07:56:07 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The charcater tpo print
+ * Return: on sucess 1
+ * on error -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
