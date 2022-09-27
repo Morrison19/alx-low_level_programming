de63585165bcaa038e660daa23fad929ea0c7356
Morrison <Ohiwereibolade72@gmail.com>
2022-09-27 06:07:30 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strchr - a function that locates a character in a string
+ * @s: contain strings of character
+ * @c: character in the sring to be found
+ * Return: NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i <= c; i++)
+	{
+		if (i != c)
+		{
+			return (NULL);
+		}
+	}
+}
+
