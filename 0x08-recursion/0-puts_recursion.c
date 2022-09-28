0c3ba4731bf0a7f9c62425a6f559928f1c317db4
Morrison <Ohiwereibolade72@gmail.com>
2022-09-28 16:30:12 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - a function that prints a string
@@ -9,7 +8,7 @@
 
 void _puts_recursion(char *s)
 {
-	if (strlen(s) < 1)
+	if (s < 1)
 		_putchar('\n');
 	else
 	{
