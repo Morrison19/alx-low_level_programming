013a51e081ac2b10f16c843b8cedc84301981eef
Morrison <Ohiwereibolade72@gmail.com>
2022-09-28 05:08:29 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,20 +2,20 @@
 
 /**
  * *_strchr - a function that locates a character in a string
- * @s: string to search
- * @c: character in the sring to be found
- * Return: NULL
+ * @s: string to be searched
+ * @c: character to be found
+ *
+ * Return: return string s or NULL
  */
 
 char *_strchr(char *s, char c)
 {
 	char *a = s - 1;
 
-	while (*a != '\0')
-	{
+	do {
+		a++;
 		if (*a == c)
 			return (a);
-		a++;
-	}
+	} while (*a != '\0')
 	return (0);
 }
