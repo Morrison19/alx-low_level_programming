ba559be9278aadcafc9b9a7aa218770235a89aa0
Morrison <Ohiwereibolade72@gmail.com>
2022-10-02 08:20:46 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of elements
+ * @argv: array of elements
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
